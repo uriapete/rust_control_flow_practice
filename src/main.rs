147c0e677d4998d0cab8cee52ebeec6d9008f927
uriapete <108434545+GP391018@users.noreply.github.com>
2023-04-09 21:39:46 -0700

    created f-c temp converter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
     println!("Hello, world!");
+    // let mode = "ctof";
+    println!("{}",convert_temps("ftoc",40.0))
+}
+
+fn convert_temps(mode:&str,input_temp:f64)->f64 {
+    if mode=="ftoc" {
+        return (input_temp-32.0)*(5.0/9.0)
+    }else if mode=="ctof" {
+        return (input_temp*1.8)+32.0
+    }else {
+        println!("Please enter a valid mode!");
+        return 1.0;
+    }
 }
