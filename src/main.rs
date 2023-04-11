fe25c5ba451ce051ab7dc12e402d66420fe1a693
uriapete <108434545+GP391018@users.noreply.github.com>
2023-04-10 20:11:47 -0700

    created fibo sequence fn and added some extra comments

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
+use std::vec;
+
 fn main() {
     println!("Hello, world!");
     // let mode = "ctof";
-    println!("{}",convert_temps("ftoc",40.0))
+    // println!("{}",convert_temps("ftoc",40.0))
+    // fibo_gen(377+610);
 }
+
+// hi, i'm learning rust
+// here's the prompt:
+
+// from "The Book" (specifically grabbing from that experiment with the quizzes and stuff)
+// To practice with [Rust's control flow], try building programs to do the following:
+
+// Convert temperatures between Fahrenheit and Celsius.
+// Generate the nth Fibonacci number.
+// Print the lyrics to the Christmas carol “The Twelve Days of Christmas,” taking advantage of the repetition in the song.
+
 
 fn convert_temps(mode:&str,input_temp:f64)->f64 {
     if mode=="ftoc" {
@@ -13,4 +27,64 @@ fn convert_temps(mode:&str,input_temp:f64)->f64 {
         println!("Please enter a valid mode!");
         return 1.0;
     }
+}
+
+// here's the fibo gen fn: print fibo nums until the max as set in args
+// this fn already prints stuff out, so no return val and no printing val of this fn
+fn fibo_gen(max_num:u128) {
+    let mut fibo_list:Vec<u128>=vec![1,2];
+
+    // i'm gonna use a vector to help with calcing fibo
+
+    // below code ended up not working
+    // i = index, num = elementnum
+    // so i can access next or previous item
+    // for (i,num) in fibo_list.iter().enumerate() {
+    //     println!("{num}");
+    //     if (i+1>=fibo_list.len()) {
+    //         let nextNum:u128=fibo_list[i-1]+num;
+    //         if nextNum<=max_num{
+    //             fibo_list.push(nextNum);
+    //         }
+    //     }
+    // }
+    // end of code that doesn't work
+
+    // i'll try a while loop this time
+
+    // current index
+    let mut curr_idx = 0;
+
+    // while index does not exceed the length of the fibo_list
+    'fibo_loop: while curr_idx<fibo_list.len() {
+        // get current num
+        let num = fibo_list[curr_idx];
+        // print current num
+        println!("{num}");
+
+        // // TESTING: print current index
+        // println!("{curr_idx}");
+
+        // if the next num does not exist (current index is equal to or exceeds length of fibo_list)
+        if (curr_idx+1)>=fibo_list.len() {
+            // calc the next num
+            let next_num:u128=fibo_list[curr_idx-1]+num;
+
+            // if the next num does not exceed the max number as set per the argument
+            if next_num<=max_num{
+                // add the next num to the list
+                fibo_list.push(next_num);
+                // continue 'fibo_loop;
+            } else {
+                // do i need to explain this line of code?
+                // fine i will anyways, if the next num does exceed the max, break the loop and finish the function, the function should return null
+                break 'fibo_loop;
+            }
+        }
+        // if code reaches here, that means it didn't break as the next num is still less than max
+        // therefore, increment the idx
+        curr_idx+=1;
+        
+    }
+    
 }
